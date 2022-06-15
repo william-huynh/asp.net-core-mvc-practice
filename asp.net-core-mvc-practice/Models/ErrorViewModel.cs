e035365571b517468dbffc2c94aef1d106a516fa
william_huynh <hnbao@WILL1AM>
2022-06-15 09:51:56 +0700

    Add project files.

---
+++ asp.net-core-mvc-practice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp.net_core_mvc_practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
